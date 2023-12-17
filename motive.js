e013e9249d072c26e7ffb0b07e6c64dd430fec95
nineret <67231866+nineret@users.noreply.github.com>
2023-12-17 23:28:49 +0700

    Create motive.js

---
+++ motive.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2023-12-17
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/nineret/motive-tools/new/main
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
